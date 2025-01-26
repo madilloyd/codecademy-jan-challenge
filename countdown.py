e35f5da405fb00e7cab0d0c689fc0b86284fe3be
madilloyd <madi.lloyd@gmail.com>
2025-01-26 16:04:27 -0700

    getting user input

--- countdown.py
+++ countdown.py
@@ -1,9 +1,17 @@
 import datetime
 # Get current time
 current_time = datetime.datetime.now()
-print(current_time.strftime("%x %I:%M:%S %p %Z"))
+#print(current_time.strftime("%x %I:%M:%S %p"))
 
 # Get event info
+event = input("Event name: ")
+event_date = input("Enter date due (mm-dd-yyyy): ")
+event_time = input("Enter time due (hh:mm): ")
+
+#print(event, "is occuring on, ", end="")
+#print(event_date, end=" ")
+#print("at", event_time)
+
 
 #Calculate time difference
 
