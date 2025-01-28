89877caadfdd198db46766f1a5cd6e3bcc374003
madilloyd <madi.lloyd@gmail.com>
2025-01-27 21:21:38 -0700

    adding time tracking elements

--- countdown.py
+++ countdown.py
@@ -1,18 +1,29 @@
-import datetime
+from datetime import datetime
+from datetime import timedelta
+from datetime import date
 # Get current time
-current_time = datetime.datetime.now()
+current_time = datetime.now()
 #print(current_time.strftime("%x %I:%M:%S %p"))
 
 # Get event info
 event = input("Event name: ")
-event_date = input("Enter date due (mm-dd-yyyy): ")
-event_time = input("Enter time due (hh:mm): ")
+#year = input("Is your event due this year? (y/n): ")
 
-#print(event, "is occuring on, ", end="")
-#print(event_date, end=" ")
-#print("at", event_time)
+event_datetime_str = input("Enter the date and time (YYYY-MM-DD HH:MM:SS): ")
+event_datetime = datetime.strptime(event_datetime_str, "%Y-%m-%d %H:%M:%S")
 
 
 #Calculate time difference
+datetime_difference = event_datetime - current_time
+#print(datetime_difference)
+
+# Extract days, hours, minutes, seconds
+days = datetime_difference.days
+total_seconds = datetime_difference.total_seconds()
+
+hours = int(total_seconds // 3600) % 24
+minutes = int(total_seconds // 60) % 60
+seconds = int(total_seconds % 60)
 
 #Dispaly countdown
+print(f"{days} days, {hours} hours, {minutes} minutes, {seconds} seconds until {event}")
